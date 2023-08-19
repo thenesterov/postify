f1c87901f1dd4317a17c507d915acfd5c7ec6cd6
thenesterov <thenesterov@proton.me>
2023-08-19 19:29:59 +0500

    feature(web): new color palette

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-2xl font-medium my-2 text-flash'>Postify</h1>
-      <h3 className='italic text-reflection'>Posting Tomorrow's Content Today!</h3>
+      <h1 className='text-2xl font-medium my-2 text-flash-500'>Postify</h1>
+      <h3 className='italic text-flash-200'>Posting Tomorrow's Content Today!</h3>
     </div>
   );
 }
